e93ba30491aa3b4a918fb83ed0afc0b7c06761ee
alangabrielordonezbenito <141080438+alangabrielordonezbenito@users.noreply.github.com>
2024-04-14 19:25:01 -0500

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'contacts-yt-78b25',
+    appId: '1:804548077266:web:63da2ad777445f09582f0b',
+    storageBucket: 'contacts-yt-78b25.appspot.com',
+    apiKey: 'AIzaSyBdOmFRpCX3G08LEGdX4bt535BbuI9oWgc',
+    authDomain: 'contacts-yt-78b25.firebaseapp.com',
+    messagingSenderId: '804548077266',
+  },
+};
